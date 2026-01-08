bad2a7222e712740a8cd9d6ec9c9873a7b6bb37b
Pedro Ramos <pedroramoscontato@hotmail.com>
2026-01-08 09:34:02 -0300

    [001] - Realizado os TODOs propostos + Herdar a classe Smartphone nas classes Nokia e Iphone + Implementar a classe abstrata InstalarAplicativo +  Realizar testes no program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nokia android = new Nokia("(48)98419-0863", "Tijolão", "sei la", 4);
+
+            android.InstalarAplicativo("Chrome");
+
+            
+            Iphone iponei = new Iphone("(48)98419-0863", "Iphone 9000", "sei la", 9999);
+
+            iponei.InstalarAplicativo("I'm rich");
+        }
+    }
+}
